bd829b001bc40ccc8f80740b275bd15d05099f0b
Divit Singh <sudosingh@ubuntu.(none)>
2015-03-21 14:22:52 -0700

    Fixed panzoom css error

--- graphs/static/graphs/js/view_graph.js
+++ graphs/static/graphs/js/view_graph.js
@@ -135,23 +135,10 @@ $(document).ready(function() {
     
     // draw graph, handle events etc.
     ready: function(){
-      var defaults = ({
-          zoomFactor: 0.05, // zoom factor per zoom tick
-          zoomDelay: 45, // how many ms between zoom ticks
-          minZoom: 0.1, // min zoom level
-          maxZoom: 10, // max zoom level
-          fitPadding: 50, // padding when fitting
-          panSpeed: 10, // how many ms in between pan ticks
-          panDistance: 50, // max pan distance per tick
-          panDragAreaSize: 75, // the length of the pan drag box in which the vector for panning is calculated (bigger = finer control of pan speed and direction)
-          panMinPercentSpeed: 0.25, // the slowest speed we can pan by (as a percent of panSpeed)
-          panInactiveArea: 8, // radius of inactive area in pan drag box
-          panIndicatorMinOpacity: 0.5, // min opacity of pan indicator (the draggable nib); scales from this to 1.0
-          autodisableForMobile: true, // disable the panzoom completely for mobile (since we don't really need it with gestures like pinch to zoom)
 
-      });
       //Adding in the panzoom functionality 
-      this.panzoom(defaults);
+      this.panzoom();
+      
       // make the selection states of the elements mutable
       this.elements().selectify();
 
