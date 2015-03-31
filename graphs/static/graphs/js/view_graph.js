30f08c87e109d69c88fbfe5344553e05e7736c71
Divit Singh <sudosingh@ubuntu.(none)>
2015-03-30 22:13:56 -0700

    Fixing edge display error

--- graphs/static/graphs/js/view_graph.js
+++ graphs/static/graphs/js/view_graph.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
         })
       .selector('edge')
         .css({
-          'target-arrow-shape': 'triangle',
+          'target-arrow-shape': 'data(arrow-shape)',
           'line-color': 'data(color)',
           'source-arrow-color': 'data(color)',
           'target-arrow-color': 'data(color)',
@@ -128,6 +128,12 @@ $(document).ready(function() {
               edgeData['color'] = addCharacterToHex(edgeData['color']);
             }
            }
+        }
+
+        if (edgeData['directed'] == false) {
+          edgeData['arrow-shape'] = 'none';
+        } else {
+          edgeData['arrow-shape'] = 'triangle';
         }
       }
 
