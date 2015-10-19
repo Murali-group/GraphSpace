7efa70f5fb28e356eaca90bdc442371061bb56bb
Divit Singh <Divit@Divits-MacBook-Pro.local>
2015-10-19 14:19:50 -0400

    Created script to install dependencies

---
+++ configure.py
@@ -0,0 +1,37 @@
+import sys
+import subprocess
+import urllib2
+import time
+import pip
+import os
+
+def get_pip():
+	response = urllib2.urlopen("https://bootstrap.pypa.io/get-pip.py")
+	code = response.read()
+	pip_file = open("get-pip.py", "w")
+	pip_file.write(code)
+	pip_file.close()
+	subprocess.check_call(["sudo", sys.executable, "get-pip.py"])
+
+def launch_server():
+	subprocess.check_output([sys.executable, "manage.py", "runserver"])
+
+def run_tests():
+	os.system("python tests/restapi_test.py")
+
+def install(package):
+    pip.main(["install", package])		
+
+if __name__ == "__main__":
+	get_pip()
+
+	install("django")
+	install("py-bcrypt")
+	install("django-analytical")
+
+	subprocess.check_call(["mv", "startup.db", "graphspace.db"])
+	subprocess.check_call([sys.executable, "manage.py", "migrate"])
+
+	print "All dependencies have successfully been installed."
+	print "\n"
+	print "**Please modify all environtment variables in gs-setup.sh and run command: '. gs-setup.sh' **"
--- gs-setup.sh
+++ gs-setup.sh
@@ -4,5 +4,5 @@ export SECRET_KEY='io#=lvucky-+r=8hif7vl7kv@y(iv6=b0sjk@x9885t(4)%2i0'
 export GOOGLE_ANALYTICS_PROPERTY_ID='UA-69001577-1'
 export EMAIL_HOST='smtp.gmail.com'
 export EMAIL_HOST_USER='graphspacevt@gmail.com'
-export EMAIL_HOST_PASSWORD='vtresearch'
+export EMAIL_HOST_PASSWORD='None'
 export EMAIL_PORT=587
