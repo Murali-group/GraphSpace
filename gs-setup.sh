52e6c7713bfdaf3321e47266bd8b1bf122c2de65
Divit Singh <Divit@Divits-MacBook-Pro.local>
2015-10-19 14:31:29 -0400

    Removed values in gs-setup.sh file and replaced with filler values.  When user downloads GraphSpace, they must change these values to their own values.

--- gs-setup.sh
+++ gs-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export SECRET_KEY='io#=lvucky-+r=8hif7vl7kv@y(iv6=b0sjk@x9885t(4)%2i0'
-export GOOGLE_ANALYTICS_PROPERTY_ID='UA-69001577-1'
+export SECRET_KEY='None'
+export GOOGLE_ANALYTICS_PROPERTY_ID='None'
 export EMAIL_HOST='smtp.gmail.com'
-export EMAIL_HOST_USER='graphspacevt@gmail.com'
+export EMAIL_HOST_USER='example@gmail.com'
 export EMAIL_HOST_PASSWORD='None'
 export EMAIL_PORT=587
