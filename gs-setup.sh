eb986d2834896729cf51656225f3e4ef26e008a9
Divit Singh <DSin52@users.noreply.github.com>
2015-10-27 11:16:27 -0400

    Removed Email port requirement from script

--- gs-setup.sh
+++ gs-setup.sh
@@ -3,6 +3,9 @@
 export SECRET_KEY='None'
 export GOOGLE_ANALYTICS_PROPERTY_ID='None'
 export EMAIL_HOST='smtp.gmail.com'
-export EMAIL_HOST_USER='example@gmail.com'
+export EMAIL_HOST_USER='None'
 export EMAIL_HOST_PASSWORD='None'
-export EMAIL_PORT=587
+
+# You should not need to modify any of the lines below
+export DEBUG=False
+export TEMPLATE_DEBUG=False
