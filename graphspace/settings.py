c2ecdef3867b87701ddef09dd8280f1bf5eaf31e
Divit Singh <Divit@Divits-MacBook-Pro.local>
2015-10-27 11:20:04 -0400

    Hardcoded email port

--- graphspace/settings.py
+++ graphspace/settings.py
@@ -84,7 +84,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_PORT = 587
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
