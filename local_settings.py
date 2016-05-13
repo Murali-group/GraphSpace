9410924427489527375624ecf2590900b499db11
Divit Singh <Divit@Divits-MacBook-Pro.local>
2016-05-13 11:13:54 -0400

    Modified readme and added comments to local_settings

--- local_settings.py
+++ local_settings.py
@@ -10,22 +10,35 @@ import os
 	All variables are imported in graphspace/settings.py
 '''
 
+# variables for setting up account through which GraphSpace emails
 EMAIL_HOST='NONE'
 EMAIL_HOST_USER='NONE'
 EMAIL_HOST_PASSWORD='NONE'
+
+# If error is thrown, display the error in the browser (ONLY FOR LOCAL MACHINES)
 DEBUG=True
 TEMPLATE_DEBUG=True
+
+# URL through which to access graphspace
 URL_PATH="http://localhost:8000/"
+
+# If tracking is enabled for GraphSpace in Google Analytics
 GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-00000000-0'
+
+# Keys given by creating a requestor account on Amazon Mechanical Turk (https://www.mturk.com/mturk/welcome)
 AWSACCESSKEYID='None'
 SECRETKEY='None'
-PATH = "path_to_graphspace/"
+
+# Path to GraphSPace
+PATH = "/Path_to_GraphSpace"
+
+# SHOULD NEVER CHANGE THIS VALUE
 SECRET_KEY= 'None'
 
 # If needing to test on production mturk account (real money)
 #AWS_URL = 'https://mechanicalturk.amazonaws.com'
 
-# Sandbox (development) MTURK url (fake money used)
+# Sandbox (development) MTURK (fake money used)
 AWS_URL = 'https://mechanicalturk.sandbox.amazonaws.com'
 
 def getEmailHost():
