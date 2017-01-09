0b15e3eb05851f8e59398613795dbcfdb690b64e
Aditya Bharadwaj <adb@vt.edu>
2017-01-09 13:13:11 -0500

    stale commit

--- graphspace/settings/base.py
+++ graphspace/settings/base.py
@@ -92,6 +92,8 @@ EMAIL_PORT = 587
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
