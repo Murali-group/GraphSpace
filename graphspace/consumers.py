f4cb4f3cd2fa08d3611d722d15cda6d7b9dea6d5
Melvin Mathew <melvin15may@gmail.com>
2017-07-15 22:41:05 -0700

    removed extra socket groups created for groups

--- graphspace/consumers.py
+++ graphspace/consumers.py
@@ -16,17 +16,6 @@ def websocket_connect(message):
             'accept': True
         })
         Group(group_name).add(message.reply_channel)
-        """
-        Group(group_name).send({
-            'accept': True
-        })
-        """
-        group_group_name = ['group-' + str(g.id) for g in db.get_groups_by_member_email(
-            Database().session(), message.http_session['uid'])]
-        message.channel_session[
-            'group_notification_group_name'] = group_group_name
-        for g in group_group_name:
-            Group(g).add(message.reply_channel)
 
 
 # Connected to websocket.keepalive
@@ -34,8 +23,6 @@ def websocket_connect(message):
 def websocket_keepalive(message):
     Group(message.channel_session['owner_notification_group_name']).add(
         message.reply_channel)
-    for g in message.channel_session['group_notification_group_name']:
-        Group(g).discard(message.reply_channel)
 
 
 # Connected to websocket.disconnect
@@ -48,6 +35,3 @@ def websocket_disconnect(message):
     if message.channel_session['owner_notification_group_name'] is not None:
         Group(message.channel_session['owner_notification_group_name']).discard(
             message.reply_channel)
-    if message.channel_session['group_notification_group_name'] is not None:
-        for g in message.channel_session['group_notification_group_name']:
-            Group(g).discard(message.reply_channel)
