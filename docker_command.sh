dc1a9b36bbcfad2dfc64790d5076cc96af3a2c62
Melvin Mathew <melvin15may@gmail.com>
2017-07-26 13:56:55 -0700

    chnages to docker commands

--- docker_command.sh
+++ docker_command.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+echo "Starting postgres"
 service postgresql start
 
+echo "Starting zookeeper"
 service zookeeper start
 sleep 2
+
+echo "Starting kafka"
 nohup ~/kafka/bin/kafka-server-start.sh ~/kafka/config/server.properties > ~/kafka/kafka.lo$
 
 sleep 2
-cat ~/kafka/kafka.log
+#cat ~/kafka/kafka.log
 
+echo "Starting redis"
 redis-server /etc/redis/redis.conf
+
+echo "Starting elasticsearch"
 su elasticsearch && /elasticsearch/bin/elasticsearch -d
 
 sleep 10
 
-cd GraphSpace && source venv/bin/activate 
+echo "Activate virtual env"
+source venv/bin/activate 
 
+echo "Migrate"
 python manage.py migrate --settings=graphspace.settings.local
 
+echo "Start GraphSpace ... "
 python manage.py runserver 0.0.0.0:8000 --settings=graphspace.settings.local
