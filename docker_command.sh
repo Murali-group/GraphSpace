debc68c0890d9797ec4b08a16d27442c6a18bd38
Melvin Mathew <melvin15may@gmail.com>
2017-07-26 20:11:15 -0700

    changes to docker compose

--- docker_command.sh
+++ docker_command.sh
@@ -4,27 +4,34 @@ service postgresql start
 
 echo "Starting zookeeper"
 service zookeeper start
+
+echo "Sleeping for 10 seconds"
 sleep 2
 
 echo "Starting kafka"
-nohup ~/kafka/bin/kafka-server-start.sh ~/kafka/config/server.properties > ~/kafka/kafka.lo$
+nohup ~/kafka/bin/kafka-server-start.sh ~/kafka/config/server.properties > ~/kafka/kafka.log 2>&1 &
 
+echo "Sleeping for 2 seconds"
 sleep 2
-#cat ~/kafka/kafka.log
+cat ~/kafka/kafka.log
 
 echo "Starting redis"
 redis-server /etc/redis/redis.conf
 
 echo "Starting elasticsearch"
-su elasticsearch && /elasticsearch/bin/elasticsearch -d
+su - elasticsearch -c '/elasticsearch/bin/elasticsearch -d'
 
+echo "Sleeping for 10 seconds"
 sleep 10
 
 echo "Activate virtual env"
-source venv/bin/activate 
+cd ./GraphSpace && source venv/bin/activate 
 
 echo "Migrate"
+#python manage.py collectstatic --noinput --settings=graphspace.settings.local
 python manage.py migrate --settings=graphspace.settings.local
+
 
 echo "Start GraphSpace ... "
 python manage.py runserver 0.0.0.0:8000 --settings=graphspace.settings.local
+
