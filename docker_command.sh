f50fa32a1e28fe7da6e92bc0a6fc3d2392523d9c
Melvin Mathew <melvin15may@gmail.com>
2017-07-28 15:53:24 -0700

    moved service start commands to docker image

--- docker_command.sh
+++ docker_command.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-echo "Starting postgres"
-service postgresql start
-
-echo "Starting zookeeper"
-service zookeeper start
-
-echo "Sleeping for 10 seconds"
-sleep 2
-
-echo "Starting kafka"
-nohup ~/kafka/bin/kafka-server-start.sh ~/kafka/config/server.properties > ~/kafka/kafka.log 2>&1 &
-
-echo "Sleeping for 2 seconds"
-sleep 2
-#cat ~/kafka/kafka.log
-
-echo "Starting redis"
-redis-server /etc/redis/redis.conf
-
-echo "Starting elasticsearch"
-su - elasticsearch -c '/elasticsearch/bin/elasticsearch -d'
-
 echo "Sleeping for 10 seconds"
 sleep 10
 
