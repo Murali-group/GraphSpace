c2669f8813edf63cc74349f12d20099c8f3eb033
Melvin Mathew <melvin15may@gmail.com>
2017-07-28 14:01:52 -0700

    changed settings file name

--- graphspace/asgi.py
+++ graphspace/asgi.py
@@ -1,7 +1,7 @@
 import os
 from channels.asgi import get_channel_layer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphspace.settings.docker")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphspace.settings.production")
 
 channel_layer = get_channel_layer()
 
