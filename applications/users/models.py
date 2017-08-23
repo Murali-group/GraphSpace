901f9fe6465085d6ac0b124f96ef88ea9fd6a8f3
Melvin Mathew <melvin15may@gmail.com>
2017-08-23 11:45:44 -0700

    merge tab problem solved

--- applications/users/models.py
+++ applications/users/models.py
@@ -29,11 +29,11 @@ class User(IDMixin, TimeStampMixin, Base):
 	owned_groups = relationship("Group", back_populates="owner", cascade="all, delete-orphan")
 	owned_graphs = relationship("Graph", back_populates="owner", cascade="all, delete-orphan")
 	owned_layouts = relationship("Layout", back_populates="owner", cascade="all, delete-orphan")
-  
-  # Notification relationships
-  receive_notification_email = Column(Boolean, default=False)
-  owned_notifications = relationship("OwnerNotification", back_populates="owner", cascade="all, delete-orphan")
-  group_notifications = relationship("GroupNotification", back_populates="group_member", cascade="all, delete-orphan")
+
+	# Notification relationships
+	receive_notification_email = Column(Boolean, default=False)
+	owned_notifications = relationship("OwnerNotification", back_populates="owner", cascade="all, delete-orphan")
+	group_notifications = relationship("GroupNotification", back_populates="group_member", cascade="all, delete-orphan")
 
 	member_groups = association_proxy('user_groups', 'group')
 
@@ -51,95 +51,92 @@ class User(IDMixin, TimeStampMixin, Base):
 			'email': cls.email,
 			'created_at': cls.created_at.isoformat(),
 			'updated_at': cls.updated_at.isoformat(),
-      'receive_notification_email': cls.receive_notification_email
+			'receive_notification_email': cls.receive_notification_email
 		}
 
 
 class PasswordResetCode(IDMixin, TimeStampMixin, Base):
-    __tablename__ = 'password_reset_code'
+	__tablename__ = 'password_reset_code'
 
-    email = Column(String, ForeignKey('user.email', ondelete="CASCADE",
-                                      onupdate="CASCADE"), nullable=False, index=True)
-    code = Column(String, nullable=False)
-    user = relationship(
-        "User", back_populates="password_reset_codes", uselist=False)
+	email = Column(String, ForeignKey('user.email', ondelete="CASCADE", onupdate="CASCADE"), nullable=False, index=True)
+	code = Column(String, nullable=False)
+	user = relationship("User", back_populates="password_reset_codes", uselist=False)
 
-    constraints = (UniqueConstraint('email', 'code',
-                                    name='_password_reset_code_uc_email_code'),)
-    indices = ()
+	constraints = (UniqueConstraint('email', 'code', name='_password_reset_code_uc_email_code'),)
+	indices = ()
 
-    @declared_attr
-    def __table_args__(cls):
-        args = cls.constraints + cls.indices
-        return args
-    
-    def serialize(cls, **kwargs):
-        return {
-          'id': cls.id,
-          'email': cls.email,
-          'code': cls.code,
-          'created_at': cls.created_at.isoformat(),
-          'updated_at': cls.updated_at.isoformat()
-        }
+	@declared_attr
+	def __table_args__(cls):
+		args = cls.constraints + cls.indices
+		return args
+	
+	def serialize(cls, **kwargs):
+		return {
+			'id': cls.id,
+			'email': cls.email,
+			'code': cls.code,
+			'created_at': cls.created_at.isoformat(),
+			'updated_at': cls.updated_at.isoformat()
+		}
 
 
 class Group(IDMixin, TimeStampMixin, Base):
-    __tablename__ = 'group'
+	__tablename__ = 'group'
 
-    name = Column(String, nullable=False)
-    owner_email = Column(String, ForeignKey('user.email', ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
-    description = Column(String, nullable=True)
-    invite_code = Column(String, nullable=False)
+	name = Column(String, nullable=False)
+	owner_email = Column(String, ForeignKey('user.email', ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
+	description = Column(String, nullable=True)
+	invite_code = Column(String, nullable=False)
 
-    owner = relationship("User", back_populates="owned_groups", uselist=False)
-    members = association_proxy('member_users', 'user')
-    graphs = association_proxy('shared_graphs', 'graph')
+	owner = relationship("User", back_populates="owned_groups", uselist=False)
+	members = association_proxy('member_users', 'user')
+	graphs = association_proxy('shared_graphs', 'graph')
 
-    # Notification relationships
-    group_notifications = relationship("GroupNotification", back_populates="group", cascade="all, delete-orphan")
+	# Notification relationships
+	group_notifications = relationship("GroupNotification", back_populates="group", cascade="all, delete-orphan")
 
-    constraints = (UniqueConstraint('name', 'owner_email', name='_group_uc_name_owner_email'),)
-    indices = ()
+	constraints = (UniqueConstraint('name', 'owner_email', name='_group_uc_name_owner_email'),)
+	indices = ()
 
-    @declared_attr
-    def __table_args__(cls):
-        args = cls.constraints + cls.indices
-        return args
+	@declared_attr
+	def __table_args__(cls):
+		args = cls.constraints + cls.indices
+		return args
 
-    def serialize(cls, **kwargs):
-        return {
-          'id': cls.id,
-          'name': cls.name,
-          'invite_code': cls.invite_code,
-          'owner_email': cls.owner_email,
-          'description': cls.description,
-          'total_graphs': len(cls.graphs),
-          'total_members': len(cls.members),
-          'created_at': cls.created_at.isoformat(),
-          'updated_at': cls.updated_at.isoformat()
-        }
+	def serialize(cls, **kwargs):
+		return {
+			'id': cls.id,
+			'name': cls.name,
+			'invite_code': cls.invite_code,
+			'owner_email': cls.owner_email,
+			'description': cls.description,
+			'total_graphs': len(cls.graphs),
+			'total_members': len(cls.members),
+			'created_at': cls.created_at.isoformat(),
+			'updated_at': cls.updated_at.isoformat()
+		}
 
 
 class GroupToUser(TimeStampMixin, Base):
-    """The class representing the schema of the group_to_user table."""
-    __tablename__ = 'group_to_user'
+	"""The class representing the schema of the group_to_user table."""
+	__tablename__ = 'group_to_user'
 
-    user_id = Column(Integer, ForeignKey('user.id', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-    group_id = Column(Integer, ForeignKey('group.id', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
+	user_id = Column(Integer, ForeignKey('user.id', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
+	group_id = Column(Integer, ForeignKey('group.id', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
 
-    user = relationship("User", backref=backref("user_groups", cascade="all, delete-orphan"), uselist=False)
-    group = relationship("Group", backref=backref("member_users", cascade="all, delete-orphan"), uselist=False)
+	user = relationship("User", backref=backref("user_groups", cascade="all, delete-orphan"), uselist=False)
+	group = relationship("Group", backref=backref("member_users", cascade="all, delete-orphan"), uselist=False)
 
-    indices = (Index('group2user_idx_user_id_group_id', 'user_id', 'group_id'),)
-    constraints = ()
+	indices = (Index('group2user_idx_user_id_group_id', 'user_id', 'group_id'),)
+	constraints = ()
 
-    @declared_attr
-    def __table_args__(cls):
-        args = cls.constraints + cls.indices
-        return args
+	@declared_attr
+	def __table_args__(cls):
+		args = cls.constraints + cls.indices
+		return args
 
-    def serialize(cls, **kwargs):
-        return {
-          'user_id': cls.user_id,
-          'group_id': cls.group_id
-        }
+	def serialize(cls, **kwargs):
+		return {
+			'user_id': cls.user_id,
+			'group_id': cls.group_id
+		}
