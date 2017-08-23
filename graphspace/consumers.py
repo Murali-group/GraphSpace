c4ec025a2ffa5a609415473fe5c77864b3b549c0
Melvin Mathew <melvin15may@gmail.com>
2017-08-23 10:51:38 -0700

    added pydoc for websocket consumer

--- graphspace/consumers.py
+++ graphspace/consumers.py
@@ -5,10 +5,13 @@ import applications.users.dal as db
 from graphspace.database import *
 
 
-# Connected to websocket.connect and websocket.keepalive
 @channel_session_user_from_http
 def websocket_connect(message):
-    # transfer_user(message.http_session, message.channel_session)
+    """
+    Establish connection to a socket
+
+    :param message: ASGI WebSocket packet-received and send-packet message
+    """
     if message.http_session['uid'] is not None:
         group_name = websocket_group_name(message.http_session['uid'])
         message.channel_session['owner_notification_group_name'] = group_name
@@ -18,9 +21,13 @@ def websocket_connect(message):
         Group(group_name).add(message.reply_channel)
 
 
-# Connected to websocket.keepalive
 @channel_session_user
 def websocket_keepalive(message):
+    """
+    Reconnect/keep-alive user socket connection
+
+    :param message: ASGI WebSocket packet-received and send-packet message
+    """
     Group(message.channel_session['owner_notification_group_name']).add(
         message.reply_channel)
 
@@ -28,7 +35,11 @@ def websocket_keepalive(message):
 # Connected to websocket.disconnect
 @channel_session_user
 def websocket_disconnect(message):
-    # transfer_user(message.http_session, message.channel_session)
+    """
+    Disconnect user socket
+
+    :param message: ASGI WebSocket packet-received and send-packet message
+    """
     message.reply_channel.send({
         'close': True
     })
