5682340a7c977a864ca66a9a358898e54b3010fc
yash <yashagarwal140@gmail.com>
2020-06-29 01:37:30 +0530

    functionality for search close and open discussions

--- applications/discussions/controllers.py
+++ applications/discussions/controllers.py
@@ -14,13 +14,35 @@ def add_discussion(request, message=None, topic=None, group_id=None, is_resolved
                                    parent_discussion_id=parent_discussion_id)
     return discussion
 
-def search_discussions_by_group_id(request, group_id=None):
+
+def search_discussions_by_group_id(request, group_id=None, topic=None, limit=20, offset=0, order='desc', sort='created_at'):
     if group_id is None:
         raise Exception("Atleast one group id is required.")
-    return db.get_discussions_by_group_id(request.db_session, group_id=group_id)
+
+    if sort == 'topic':
+        sort_attr = db.Discussion.topic
+    else:
+        sort_attr = db.Discussion.created_at
+
+    if order == 'desc':
+        orber_by = db.asc(sort_attr)
+    else:
+        orber_by = db.desc(sort_attr)
+
+    total, discussions = db.get_discussions_by_group_id(request.db_session,
+                                                        group_id=group_id,
+                                                        topic=topic,
+                                                        limit=limit,
+                                                        offset=offset,
+                                                        order_by=orber_by)
+
+    return total, discussions
+
+
 
 def get_discussion_by_id(request, discussion_id):
     return db.get_discussion(request.db_session, id=discussion_id)
+
 
 def search_comments_by_discussion_id(request, group_id=None, discussion_id=None):
     if group_id is None:
@@ -28,3 +50,34 @@ def search_comments_by_discussion_id(request, group_id=None, discussion_id=None)
     if discussion_id is None:
         raise Exception("Atleast one discussion id is required.")
     return db.get_comments_by_discussion_id(request.db_session, group_id=group_id, discussion_id=discussion_id)
+
+
+def is_user_authorized_to_delete_discussion(request, username, discussion_id):
+    is_authorized = False
+
+    discussion = db.get_discussion(request.db_session, discussion_id)
+
+    if discussion is not None:  # Graph exists
+        if discussion.owner_email == username:
+            is_authorized = True
+
+    return is_authorized
+
+
+def delete_discussion_by_id(request, discussion_id):
+    db.delete_discussion(request.db_session, id=discussion_id)
+    return
+
+
+def update_discussion(request, discussion_id, message, is_resolved):
+    discussion = {}
+    if message is not None:
+        discussion['message'] = message
+        return db.update_discussion(request.db_session, id=discussion_id, updated_discussion=discussion)
+    if is_resolved is not None:
+        if is_resolved == u'1':
+            discussion['is_resolved'] = is_resolved
+            return db.resolve_discussion(request.db_session, id=discussion_id, updated_discussion=discussion)
+        if is_resolved == u'0':
+            discussion['is_resolved'] = is_resolved
+            return db.reopen_discussion(request.db_session, id=discussion_id, updated_discussion=discussion)
--- applications/discussions/dal.py
+++ applications/discussions/dal.py
@@ -18,10 +18,57 @@ def add_discussion(db_session, message, topic, group_id, owner_email=None, is_re
 
 
 @with_session
-def get_discussions_by_group_id(db_session, group_id):
+def get_discussions_by_group_id(db_session, group_id, topic, limit, offset, order_by=desc(Discussion.created_at)):
+	# query = db_session.query(Group)
+	#
+	# if order_by is not None:
+	# 	query = query.order_by(order_by)
+	#
+	# if owner_email is not None:
+	# 	query = query.filter(Group.owner_email.ilike(owner_email))
+	#
+	# if name is not None:
+	# 	query = query.filter(Group.name.ilike(name))
+	#
+	# if description is not None:
+	# 	query = query.filter(Group.description.ilike(description))
+	#
+	# if member_email is not None:
+	# 	query = query.options(joinedload('member_users'))
+	# 	query = query.filter(Group.members.any(User.email == member_email))
+	#
+	# if graph_ids is not None and len(graph_ids) > 0:
+	# 	query = query.options(joinedload('shared_graphs'))
+	# 	query = query.filter(Group.graphs.any(Graph.id.in_(graph_ids)))
+	#
+	# total = query.count()
+	#
+	# if offset is not None and limit is not None:
+	# 	query = query.limit(limit).offset(offset)
+	#
+	# return total, query.all()
+
 	query = db_session.query(Discussion).filter(Discussion.group_id == group_id)
 	query = query.filter(Discussion.parent_discussion_id == None)
-	return query.count(), query.all()
+	if order_by is not None:
+		query = query.order_by(order_by)
+	if topic is not None:
+		query1 = query.filter(Discussion.topic.ilike(topic))
+		query2 = query.filter(Discussion.owner_email.ilike(topic))
+		query3 = query.filter(Discussion.message.ilike(topic))
+		query4 = query1.union(query2)
+		query = query3.union(query4)
+	# query = query.filter(Discussion.topic.ilike(topic))
+	# query2 = query.filter(Discussion.owner_email.ilike('%wano%'))
+	# query = query1.union(query2)
+	# query = query.order_by(desc(Discussion.created_at))
+	total = query.count()
+
+	if offset is not None and limit is not None:
+		query = query.limit(limit).offset(offset)
+
+	return total, query.all()
+	# return query.count(), query.all()
 
 @with_session
 def get_discussion(db_session, id):
@@ -37,10 +84,74 @@ def get_comments_by_discussion_id(db_session, group_id, discussion_id):
 	query = query.filter(Discussion.parent_discussion_id == discussion_id)
 	return query.count(), query.all()
 
+@with_session
+def delete_discussion(db_session, id):
+	"""
+	Delete group from Group table.
+	:param db_session: Database session.
+	:param id: Unique ID of the group
+	:return: None
+	"""
+	discussion = db_session.query(Discussion).filter(Discussion.id == id).one_or_none()
+	query = db_session.query(Discussion).filter(Discussion.parent_discussion_id == id).all()
+	db_session.delete(discussion)
+	for ele in query:
+		db_session.delete(ele)
+	return discussion
+
+@with_session
+def update_discussion(db_session, id, updated_discussion):
+	"""
+	Update group row entry.
+	:param db_session: Database session.
+	:param id: Unique ID of the group
+	:param updated_group: Updated group row entry
+	:return: Group if id exists else None
+	"""
+	discussion = db_session.query(Discussion).filter(Discussion.id == id).one_or_none()
+	for (key, value) in updated_discussion.items():
+		setattr(discussion, key, value)
+	send_discussion(discussion, event="edited")
+	return discussion
+
+@with_session
+def resolve_discussion(db_session, id, updated_discussion):
+	"""
+	Update group row entry.
+	:param db_session: Database session.
+	:param id: Unique ID of the group
+	:param updated_group: Updated group row entry
+	:return: Group if id exists else None
+	"""
+	discussion = db_session.query(Discussion).filter(Discussion.id == id).one_or_none()
+	for (key, value) in updated_discussion.items():
+		setattr(discussion, key, value)
+	send_discussion(discussion, event="resolve")
+	return discussion
+
+@with_session
+def reopen_discussion(db_session, id, updated_discussion):
+	"""
+	Update group row entry.
+	:param db_session: Database session.
+	:param id: Unique ID of the group
+	:param updated_group: Updated group row entry
+	:return: Group if id exists else None
+	"""
+	discussion = db_session.query(Discussion).filter(Discussion.id == id).one_or_none()
+	for (key, value) in updated_discussion.items():
+		setattr(discussion, key, value)
+	send_discussion(discussion, event="reopen")
+	return discussion
+
 
 @event.listens_for(Discussion, 'after_insert')
 def update_listener(mapper, connection, discussion):
 	send_discussion(discussion, event="insert")
+
+@event.listens_for(Discussion, 'after_delete')
+def delete_listener(mapper, connection, discussion):
+	send_discussion(discussion, event="delete")
 
 def send_discussion(discussion, event):
 	users_list = get_users_by_group(Database().session(), discussion.group_id)
