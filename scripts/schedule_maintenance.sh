cd6fec54a0bbc2006aee110dfe2ba3eb73f4e15e
Joseph D'Anna <jdd1@vt.edu>
2021-01-04 14:21:12 -0500

    Modified the script to take in args for Scheduled_Maintenance, Maintenance_Start_Date, Maintenance_End_Date

--- scripts/schedule_maintenance.sh
+++ scripts/schedule_maintenance.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+#run with "source schedule_maintenance.sh [arg1] [arg2] [arg3]"
+#pass dates as "m/d/y@hour:min"
+if (( $# > 0 ))
+then
+  if (($[$1 == true] || $[$1 == false]))
+  then
+    export IS_MAINTENANCE_SCHEDULED=$1
+    echo $IS_MAINTENANCE_SCHEDULED
+  else
+    printf "The value of arg1:IS_MAINTENANCE_SCHEDULED must be a boolean\n"
+  fi
+else
+  printf "Not enough arguements. Please enter arguements [IS_MAINTENANCE_SCHEDULED] [MAINTENANCE_START_DATETIME] [MAINTENANCE_END_DATETIME]\n"
+fi
 
-export IS_MAINTENANCE_SCHEDULED=$1
-if (( $# > 1 ))
+if (( $# > 1))
 then
   export MAINTENANCE_START_DATETIME=$2
-else
+  echo $MAINTENANCE_START_DATETIME
+fi
+
+if (( $# > 2))
+then
   export MAINTENANCE_END_DATETIME=$3
+  echo $MAINTENANCE_END_DATETIME
 fi
