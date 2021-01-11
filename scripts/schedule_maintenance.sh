1517430e12523d475172c7a63bc5447075ea75bd
Joseph D'Anna <jdd1@vt.edu>
2021-01-11 13:55:35 -0500

    Changed schedule_maintenance.sh to display a proper message and fixed arguement checking logic. Fixed the logic when reading in the environment variables and checking their boolean value in schedule_maintenance.html because the environment variable is a string but we want it be a boolean value in the python logic.

--- scripts/schedule_maintenance.sh
+++ scripts/schedule_maintenance.sh
@@ -3,25 +3,25 @@
 #pass dates as "m/d/y@hour:min"
 if (( $# > 0 ))
 then
-  if (($[$1 == true] || $[$1 == false]))
-  then
+  if [ $1 == "true" ]; then
     export IS_MAINTENANCE_SCHEDULED=$1
-    echo $IS_MAINTENANCE_SCHEDULED
+    echo "Schedule Maintenance Message has been enabled."
+  elif [ $1 == "false" ]; then
+    export IS_MAINTENANCE_SCHEDULED=$1
+    echo "Schedule Maintenance Message has been disabled."
   else
-    printf "The value of arg1:IS_MAINTENANCE_SCHEDULED must be a boolean\n"
+    printf "The value of arg1:IS_MAINTENANCE_SCHEDULED must be a boolean.\n"
   fi
 else
   printf "Not enough arguements. Please enter arguements [IS_MAINTENANCE_SCHEDULED] [MAINTENANCE_START_DATETIME] [MAINTENANCE_END_DATETIME]\n"
 fi
 
-if (( $# > 1))
-then
+if (( $# > 1)); then
   export MAINTENANCE_START_DATETIME=$2
-  echo $MAINTENANCE_START_DATETIME
+  echo "Scheduled Maintenance starts at " $MAINTENANCE_START_DATETIME
 fi
 
-if (( $# > 2))
-then
+if (( $# > 2)); then
   export MAINTENANCE_END_DATETIME=$3
-  echo $MAINTENANCE_END_DATETIME
+  echo "Schedule Maintenance ends at " $MAINTENANCE_END_DATETIME
 fi
