0f60e8b35e22dde36512d2a64a9f9c5a9aeb0294
Joseph D'Anna <35696537+jddanna@users.noreply.github.com>
2021-02-24 12:20:47 -0500

    Update base.py
    not using dotenv currently

--- graphspace/settings/base.py
+++ graphspace/settings/base.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 from sqlalchemy.ext.declarative import declarative_base
 
-from dotenv import load_dotenv
 import os
 from elasticsearch import Elasticsearch
 
